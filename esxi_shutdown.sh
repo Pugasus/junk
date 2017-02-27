ba8488f2218223e998ea280af2d88189c5322a29
Pugasus <alexperusco@gmail.com>
2017-02-27 23:42:02 +1100

    Update esxi_shutdown.sh

--- esxi_shutdown.sh
+++ esxi_shutdown.sh
@@ -15,8 +15,11 @@
 echo "Beginning shutdown of VMs..."
 
 #/bin/vmware-autostart.sh stop
-# Auto start/stop is dumb and won't shutdown VMs
-# that were powered on manually
+# VMwares auto start/stop is dumb and won't shutdown all VMs
+# It only shutdowns VMs that were set for auto start
+#
+# Instead, iterate through each VM and check if it is running
+# Run guest shutdown if it is
 
 for vm in $(vim-cmd vmsvc/getallvms | sed 's/[A-z].*//g')
 do
@@ -26,7 +29,7 @@ do
         fi
 done
 
-
+# Wait a few seconds, then loop and check if any VMs are still waiting to shutdown
 sleep 5
 while vm-support -V | grep 'Running'
 do
